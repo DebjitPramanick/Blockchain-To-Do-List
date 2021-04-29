0049dc31ac4b5d9b1e4841f08ed22dbb6b99a5b5
DebjitPramanick <dpramanick9990@gmail.com>
2021-04-29 08:25:45 +0530

    UI created

---
+++ src/main.js
@@ -0,0 +1,59 @@
+App = {
+    loading: false,
+    contracts: {},
+    load: async()=>{
+        await App.loadWeb3()
+        await App.loadAccount()
+        await App.loadContract()
+    },
+
+    loadWeb3: async () => {
+        if (typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider
+            web3 = new Web3(web3.currentProvider)
+        } else {
+            window.alert("Please connect to Metamask.")
+        }
+        // Modern dapp browsers...
+        if (window.ethereum) {
+            window.web3 = new Web3(ethereum)
+            try {
+                // Request account access if needed
+                await ethereum.enable()
+                // Acccounts now exposed
+                web3.eth.sendTransaction({/* ... */ })
+            } catch (error) {
+                // User denied account access...
+            }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+            App.web3Provider = web3.currentProvider
+            window.web3 = new Web3(web3.currentProvider)
+            // Acccounts always exposed
+            web3.eth.sendTransaction({/* ... */ })
+        }
+        // Non-dapp browsers...
+        else {
+            console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+        }
+    },
+
+    loadAccount: async() => {
+        window.ethereum.enable().then(accounts => {
+            App.account = accounts[0]
+        })
+        
+    },
+    loadContract: async() => {
+        const todoList = await (await fetch('TodoList.json')).text()
+        App.contracts.TodoList = TruffleContract(todoList)
+        App.contracts.TodoList.setProvider(App.web3Provider)
+
+        App.todoList = await App.contracts.TodoList.deployed()
+    }
+}
+
+window.addEventListener('load', () => {
+    App.load()
+})
